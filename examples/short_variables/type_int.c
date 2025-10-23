cb04fdd7deadeeaa5bc2bf793291ee4f0d346213
bi3mer <colan@comcast.net>
2025-10-23 15:04:56 -0400

    ADJUST_INT

--- examples/short_variables/type_int.c
+++ examples/short_variables/type_int.c
@@ -1,6 +1,29 @@
+#include "adjust.h"
 #include <stdio.h>
+#include <unistd.h>
+
+int get_int(void);
 
 int main(void)
 {
-    printf("Short lifetime variables not yet supported!\n");
+    adjust_init();
+
+    for (size_t countdown = 5; countdown > 0; countdown--)
+    {
+        printf("%lu...\n", countdown);
+        sleep(1);
+    }
+
+    printf("Before: %i\n", get_int());
+    adjust_update();
+    printf("After: %i\n", get_int());
+
+    adjust_cleanup();
+
+    return 0;
+}
+
+int get_int(void)
+{
+    return ADJUST_INT(-4000);
 }
